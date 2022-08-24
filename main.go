495698c05a942fefaf8aa1179a98e218323e70ca
Philip C <philipintech@gmail.com>
2022-08-24 00:51:10 -0400

    added deal function

--- main.go
+++ main.go
@@ -1,12 +1,21 @@
 package main
 
+import "fmt"
+
 func main() {
-	cards := deck{"Ace of Diamonds", newCard()}
-	cards = append(cards, "Six of Spades")
+	// cards := deck{"Ace of Diamonds", newCard()}
+	// cards = append(cards, "Six of Spades")
+	// cards.print()
+	cards := newDeck()
 
-	cards.print()
+	hand, remainingCards := deal(cards, 5)
+	hand.print()
+	fmt.Println("Remaining Hand is....")
+	remainingCards.print()
 }
 
-func newCard() string{
-	return "Five of Diamonds"
-}
+// func newCard() string {
+// 	return "Five of Diamonds"
+// }
+
+// finished at video # 23
