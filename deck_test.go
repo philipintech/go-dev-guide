b575f3b07bed35c8b9652f1285cc6b5008beb6b6
Philip C <philipintech@gmail.com>
2022-09-12 23:39:52 -0400

    added TestNewDeck function

--- deck_test.go
+++ deck_test.go
@@ -1,1 +1,14 @@
 package main
+
+import "testing"
+
+func TestNewDeck(t *testing.T) {
+	d := newDeck()
+
+	if len(d) != 52 {
+		t.Errorf("Expected deck length of 52, but got ", len(d))
+	}
+
+}
+
+//
